ec467183beac939938710cba064aa4d329309100
LEENA SRI M P <leenasripslg14@gmail.com>
2024-07-29 21:59:42 +0530

    Create PaymentContext.java

---
+++ Week1_Design_pattern_and_principles/Implementing the Strategy Pattern/PaymentContext.java
@@ -0,0 +1,16 @@
+
+public class PaymentContext {
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void pay(double amount) {
+        if (paymentStrategy != null) {
+            paymentStrategy.pay(amount);
+        } else {
+            System.out.println("Payment strategy not set.");
+        }
+    }
+}
