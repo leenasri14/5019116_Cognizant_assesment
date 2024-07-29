909f2ff75d9c6c00ec0a7a42d4a7c109a2872a10
LEENA SRI M P <leenasripslg14@gmail.com>
2024-07-29 18:55:09 +0530

    Create Search.java

---
+++ Week1_Algorithms_and_generation/E-commerce Platform Search Function/Search.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+class Search {
+
+    // Linear search
+    public static Product linearSearch(Product[] products, String productName) {
+        for (Product product : products) {
+            if (product.getProductName().equalsIgnoreCase(productName)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    // Binary search
+    public static Product binarySearch(Product[] products, String productName) {
+        int left = 0;
+        int right = products.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int comparison = products[mid].getProductName().compareToIgnoreCase(productName);
+
+            if (comparison == 0) {
+                return products[mid];
+            } else if (comparison < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return null;
+    }
+
+    // Sort products by productName for binary search
+    public static void sortProductsByName(Product[] products) {
+        Arrays.sort(products, (p1, p2) -> p1.getProductName().compareToIgnoreCase(p2.getProductName()));
+    }
+}
