dcf090eb843709eb99d2d6ed30a09a0c3863356d
LEENA SRI M P <leenasripslg14@gmail.com>
2024-07-29 19:05:05 +0530

    Create SortingAlgorithms.java

---
+++ Week1_Algorithms_and_generation/Sorting_Customer_Orders/SortingAlgorithms.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class SortingAlgorithms {
+
+    // Bubble Sort
+    public static void bubbleSort(Order[] orders) {
+        int n = orders.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (orders[j].getTotalPrice() > orders[j + 1].getTotalPrice()) {
+                    Order temp = orders[j];
+                    orders[j] = orders[j + 1];
+                    orders[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Quick Sort
+    public static void quickSort(Order[] orders, int low, int high) {
+        if (low < high) {
+            int pi = partition(orders, low, high);
+            quickSort(orders, low, pi - 1);
+            quickSort(orders, pi + 1, high);
+        }
+    }
+
+    private static int partition(Order[] orders, int low, int high) {
+        Order pivot = orders[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (orders[j].getTotalPrice() < pivot.getTotalPrice()) {
+                i++;
+                Order temp = orders[i];
+                orders[i] = orders[j];
+                orders[j] = temp;
+            }
+        }
+        Order temp = orders[i + 1];
+        orders[i + 1] = orders[high];
+        orders[high] = temp;
+        return i + 1;
+    }
+}
+
