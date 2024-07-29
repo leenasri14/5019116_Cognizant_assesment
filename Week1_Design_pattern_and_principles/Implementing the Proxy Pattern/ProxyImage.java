1221086667461bf65d64b892aa6f0fe5be2fd79f
LEENA SRI M P <leenasripslg14@gmail.com>
2024-07-29 21:50:36 +0530

    Create  ProxyImage.java

---
+++ Week1_Design_pattern_and_principles/Implementing the Proxy Pattern/ProxyImage.java
@@ -0,0 +1,17 @@
+
+public class ProxyImage implements Image {
+    private String imageUrl;
+    private RealImage realImage;
+
+    public ProxyImage(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(imageUrl);
+        }
+        realImage.display();
+    }
+}
